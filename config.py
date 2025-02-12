c4a6906f2527018b91fdae4810ecf79faafb63a3
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2025-02-12 20:18:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     API_HASH = "42a60d9c657b106370c79bb0a8ac560c"  
     TOKEN = os.environ.get("TOKEN", "YOUR BOT TOKEN")
     MONGO_URL = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
-    START_PIC = "https://files.catbox.moe/g2leyq.jpg"
+    START_PIC = "https://envs.sh/OTc.jpg"
     SUDOERS = filters.user(["5186826758"])
