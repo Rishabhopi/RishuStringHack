6769110854db327ffb5b890f58a36a787b538324
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2025-02-12 20:09:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "YOUR API ID"
-    API_HASH = "YOUR API HASH"  
+    API_ID = "14050586"
+    API_HASH = "42a60d9c657b106370c79bb0a8ac560c"  
     TOKEN = os.environ.get("TOKEN", "YOUR BOT TOKEN")
-    MONGO_URL = "YOUR MANGO DB URL"
+    MONGO_URL = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://files.catbox.moe/g2leyq.jpg"
     SUDOERS = filters.user(["YOUR OWNER ID"])
